11dbc5a9ae208877f125dbd49d15a88dc2ffc5f6
Taylor Doughty <t.m.doughty@student.vu.nl>
2023-04-03 16:17:47 +0200

    update on clustering.py
    having some errors with TransE atm but will look into them :-)

--- clustering.py
+++ clustering.py
@@ -1,45 +1,69 @@
-# To use Scikit-learn (sklearn) and PyKEEN to perform KMeans clustering over a knowledge graph, you can follow these steps:
+# # load the knowledge graph
+# # knowledge_graph = from_path(r'C:\Users\dough\Documents\GitHub\KRoW-Project\data\Project_onto.owl')
+# knowledge_graph = pykeen.triples.utils.load_triples(r'C:\Users\dough\Documents\GitHub\KRoW-Project\data\Project_onto.owl')
+# # pykeen.triples.utils.load_triples()
+# # extract features
 
-# Load the Knowledge Graph: First, you need to load the knowledge graph into a PyKEEN-compatible format. PyKEEN can load knowledge graphs 
-# from various sources such as RDF triples, TSV files, and more. You can use the PyKEEN API to load your knowledge graph into a PyKEEN-compatible 
-# format.
+# KG = pykeen.triples.TriplesFactory(knowledge_graph, entity_to_id = None, relation_to_id = None)
+# model = pykeen.models.TransE(triples_factory = KG)
+# model.fit(knowledge_graph)
+# features = model.get_all_entities()
 
-# Extract Features: After loading the knowledge graph, you need to extract features for each entity in the graph. PyKEEN provides several 
-# methods for feature extraction such as TransE, RotatE, and more. You can use these methods to extract features for each entity.
+# # train the KMeans model
+# kmeans = KMeans(n_clusters=3, random_state=0)
+# kmeans.fit(features)
 
-# Train the KMeans Model: Once you have extracted the features, you can use the Scikit-learn API to train a KMeans model. You can pass the 
-# extracted features to the KMeans model and specify the number of clusters you want to create.
+# # evaluate the model
+# silhouette_coefficient = sklearn.metrics.silhouette_score(features, kmeans.labels_)
+# inertia = kmeans.inertia_
 
-# Evaluate the Model: After training the KMeans model, you can evaluate its performance by calculating metrics such as the Silhouette 
-# Coefficient or Inertia.
-
-# Here's an example code snippet that shows how to use PyKEEN and Scikit-learn to perform KMeans clustering over a knowledge graph:
+# print(f"Silhouette Coefficient: {silhouette_coefficient}")
+# print(f"Inertia: {inertia}")
 
 import pykeen
-from sklearn.cluster import KMeans
+import numpy as np
 from pykeen import utils, triples, models
-import sklearn 
-import numpy
+from sklearn.cluster import KMeans
+from rdflib import Graph, ConjunctiveGraph, Literal, BNode, Namespace, RDF, URIRef, Literal, OWL, RDFS
+from pykeen.pipeline import pipeline
+from pykeen.models import TransE
+
+g = Graph()
+g.parse(r'C:\Users\dough\Documents\GitHub\KRoW-Project\data\Project')
+# g.parse("./data/Project")
+# from krw_ml.py
+data = []
+i=0
+for s,p,o in g.triples((None,None,None)):
+    if '#' in s and '#' in p and '#' in o:
+        data.append([s.split("#")[1], p.split("#")[1], o.split("#")[1]])
+print(data[:10])
+t = np.array(data, dtype=str)
+
+trip = triples.TriplesFactory.from_labeled_triples(t)
+training, testing = trip.split([0.95,0.05])
+
+# path_to_kg = (r'C:\Users\dough\Documents\GitHub\KRoW-Project\data\Project.ttl')
+# triples_factory = pykeen.triples.TriplesFactory.from_path(path=path_to_kg, create_inverse_triples=False)
 
 
-# load the knowledge graph
-# knowledge_graph = from_path(r'C:\Users\dough\Documents\GitHub\KRoW-Project\data\Project_onto.owl')
-knowledge_graph = pykeen.triples.utils.load_triples(r'C:\Users\dough\Documents\GitHub\KRoW-Project\data\Project_onto.owl')
-# pykeen.triples.utils.load_triples()
-# extract features
+embedding_model = pykeen.models.TransE(triples_factory = trip)
 
-KG = pykeen.triples.TriplesFactory(knowledge_graph)
-model = pykeen.models.TransE(triples_factory = KG)
-model.fit(knowledge_graph)
-features = model.get_all_entities()
+result = pipeline(
+    random_seed = 0,
+    model = embedding_model,
+    training_kwargs = dict(num_epochs = 2),
+    evaluation_kwargs = dict(),
+    training=training,
+    testing=testing,)
 
-# train the KMeans model
-kmeans = KMeans(n_clusters=3, random_state=0)
-kmeans.fit(features)
+# Get the entity embeddings
+entity_embeddings = result.model.get_all_entities()[0].detach().numpy()
 
-# evaluate the model
-silhouette_coefficient = sklearn.metrics.silhouette_score(features, kmeans.labels_)
-inertia = kmeans.inertia_
+# Cluster the embeddings using k-means
+num_clusters = 5  # Set the number of clusters
+kmeans = KMeans(n_clusters=num_clusters, random_state=0)
+cluster_labels = kmeans.fit_predict(entity_embeddings)
 
-print(f"Silhouette Coefficient: {silhouette_coefficient}")
-print(f"Inertia: {inertia}")
+# Print the cluster labels
+print("Cluster labels: ", cluster_labels)
