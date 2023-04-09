95448e36b4744916f9e1ca737b47567f10b2c90f
NMoolhuijsen <95701858+NMoolhuijsen@users.noreply.github.com>
2023-04-09 21:56:13 +0200

    Update krw_ml.py

--- krw_ml.py
+++ krw_ml.py
@@ -1,23 +1,18 @@
 # you need to install pykeen beforehand, see https://pykeen.readthedocs.io/en/stable/installation.html 
 import numpy as np
 import pandas as pd
-import seaborn as sb
-
-from pykeen.datasets import Nations
+from pykeen.pipeline import pipeline
 from pykeen import triples
-
-import rdflib
 from rdflib import Graph, ConjunctiveGraph, Literal, BNode, Namespace, RDF, URIRef, Literal, OWL, RDFS
-from rdflib.namespace import DC, FOAF
 import matplotlib.pyplot as plt
+import os
 
-# parse the ontology into a graph
+# parse the KG into a graph
 g = Graph()
-g.parse("./data/Project.ttl")
+g.parse("data/KG.ttl")
 
 ############################# create triples from graph; should be np.array ########################################
 
-# data = {'subject':[], 'predicate':[], 'object':[]}
 data = []
 i=0
 for s,p,o in g.triples((None,None,None)):
@@ -30,103 +25,94 @@ for s,p,o in g.triples((None,None,None)):
     data.append([s, p, o])
 t = np.array(data, dtype=str)
 
-
-############################################### ML + CLUSTERING  ###################################################
-
-# ml and clustering from https://docs.ampligraph.org/en/1.1.0/tutorials/ClusteringAndClassificationWithEmbeddings.html
-# not finished, issues with import statements
-# packages necessary: tensorflow, ampligraph, ..
-import ampligraph
-from ampligraph.evaluation import train_test_split_no_unseen
-
-X_train, X_valid = train_test_split_no_unseen(t, test_size=5)
-
-from ampligraph.latent_features import ComplEx
-
-# prediction model
-model = ComplEx(batches_count=10,
-                epochs=300,
-                k=100,
-                eta=20,
-                optimizer='adam', 
-                optimizer_params={'lr':1e-4},
-                loss='multiclass_nll',
-                regularizer='LP', 
-                regularizer_params={'p':3, 'lambda':1e-5}, 
-                seed=0, 
-                verbose=True)
-
-# train model
-model.fit(X_train)
-
-# filter out false negatives generted by the corruption strategy
-filter_triples = np.concatenate((X_train, X_valid))
-
-# evaluate performance
-from ampligraph.evaluation import evaluate_performance
-
-ranks = evaluate_performance(X_valid,
-                             model=model, 
-                             filter_triples=filter_triples,
-                             use_default_protocol=True,
-                             verbose=True)
-
-from ampligraph.evaluation import mr_score, mrr_score, hits_at_n_score
-
-mr = mr_score(ranks)
-mrr = mrr_score(ranks)
-
-print("MRR: %.2f" % (mrr))
-print("MR: %.2f" % (mr))
-
-#hits at n score: the probability that the model will put the correct value
-# in the thop n possibilities
-hits_10 = hits_at_n_score(ranks, n=10)
-print("Hits@10: %.2f" % (hits_10))
-hits_3 = hits_at_n_score(ranks, n=3)
-print("Hits@3: %.2f" % (hits_3))
-hits_1 = hits_at_n_score(ranks, n=1)
-print("Hits@1: %.2f" % (hits_1))
-
-
 ############################################ ML as in tutorial ##############################################
 
-# transform data into triples factory
+# TRIPLES
 trip = triples.TriplesFactory.from_labeled_triples(t)
 
-#split data into test and train set
+# SPLIT DATA INTO TRAIN AND TEST
 training, testing = trip.split([0.95,0.05])
 
 print('Train set size: ', training.triples.shape)
 print('Test set size: ', testing.triples.shape)
 
-from pykeen.pipeline import pipeline
 
-# here we don't import the model, but let PyKEEN do the importing.
+# CREATE PIPELINE
 pipeline_result = pipeline(
     random_seed=0,
-    model='HolE',
-    training=training,
-    testing=testing,
-    #  training_kwargs=dict(
-    #     num_epochs=200,
-    #     checkpoint_name='got_complex_checkpoint.pt',
-    #     checkpoint_directory='checkpoint_dir/',
-    #     checkpoint_frequency=20,
-    # ),
-    # dimensions=150,
-    # optimizer='adam',
-    # optimizer_kwargs={'lr':1e-3},
-    # loss='pairwisehinge', 
-    # regularizer='LP', 
-    # regularizer_kwargs={'p':3, 'weight':1e-5}, 
-    # negative_sampler='basic',
-    # negative_sampler_kwargs=dict(
-    #     filtered=True,
-    # )
+    model='ComplEx',                # CHANGE MODEL:
+    dimensions=150,                         # elise: ComplEx
+    training=training,                      # Josip: TransE
+    testing=testing,                        # taylor: RotatE
+     training_kwargs=dict(                  # nikki: DistMult
+        num_epochs=1,                                          # epochs: 50 and 100
+        checkpoint_name='checkpoint.pt',
+        checkpoint_directory='checkpoint_dir/',
+        checkpoint_frequency=20,
+    ),
+    optimizer='adam',                   
+    optimizer_kwargs={'lr':1e-3},
+    loss='pairwisehinge',               
+    regularizer='LP', 
+    regularizer_kwargs={'p':3, 'weight':1e-5}, 
+    negative_sampler='basic',
+    negative_sampler_kwargs=dict(
+        filtered=True,
+    )
 )
-pipeline_result.plot_losses()
-plt.show()
-print(pipeline_result.get_metric('mrr'))
+
+# RESULTS --> CHANGE NAME AND NR. EPOCHS
+pipeline_result.plot_losses()                           
+print(pipeline_result.get_metric('mrr'))            # inverse_harmonic_mean_rank in results
 print(pipeline_result.get_metric('hits@10'))
-pipeline_result.save_to_directory("./data/results")
+pipeline_result.save_to_directory("ComplEx_50")     # CHANGE TO YOUR MODEL AND NR. EPOCHS
+
+plt.savefig('ComplEx_50_losses.png')                # CHANGE TO YOUR MODEL AND NR. EPOCHS
+plt.show()  
+
+############################################ MISSING LINK PREDICTION ##################################################
+from pykeen import predict
+from scipy.special import expit
+
+# all drugs
+drugcodes = []
+symptoms = []
+for triple in data:
+    if 'resultedIn' in triple[1]:
+        if triple[0] not in drugcodes:
+            drugcodes.append(triple[0])
+        if triple[2] not in symptoms:
+            symptoms.append(triple[2])
+
+links = []
+for drug in drugcodes:
+    for symptom in symptoms:
+        links.append([drug, 'resultedIn', symptom])
+
+X_unseen = np.array(links)
+
+# from pykeen.predict import predict_triples
+
+# got_unseen = triples.get_mapped_tripples(X_unseen,factory=got)
+pack = predict.predict_triples(model=pipeline_result.model, triples=X_unseen, triples_factory=trip)
+
+processed_results = pack.process().df
+# print(processed_results)
+
+probs = expit(processed_results['score'])
+# print(probs)
+
+processed_results['prob'] = probs
+processed_results['triple'] = list(zip([' '.join(x) for x in X_unseen]))
+
+# processed_results
+res = pd.DataFrame(list(zip([' '.join(x) for x in X_unseen],  
+                      np.squeeze(processed_results['score']),
+                      np.squeeze(probs))), 
+             columns=['statement', 'score', 'prob']).sort_values("score")
+
+res.to_pickle("ComplEx_50.pkl")     # CHANGE TO YOUR MODEL AND NR. EPOCHS
+
+## to read table
+# df = pd.read_pickle("ComplEx_50.pkl")
+# print(df)
