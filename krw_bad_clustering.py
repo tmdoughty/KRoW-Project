5f02e9c90aa1ba1174ee8202f07b1ec73dc164d8
NMoolhuijsen <95701858+NMoolhuijsen@users.noreply.github.com>
2023-04-12 18:13:07 +0200

    clean louvain cluster

--- krw_bad_clustering.py
+++ krw_bad_clustering.py
@@ -1,21 +1,17 @@
 import pandas as pd
 import rdflib
-from rdflib import Graph, ConjunctiveGraph, Literal, BNode, Namespace, RDF, URIRef, Literal, OWL, RDFS
-from rdflib.namespace import DC, FOAF
+from rdflib import Graph, OWL
 from rdflib.extras.external_graph_libs import rdflib_to_networkx_digraph
 import networkx as nx
-from owlready2 import get_ontology, Thing, DataProperty, ObjectProperty
 import matplotlib.pyplot as plt
-import re
 import networkx.algorithms.community as nx_comm
 
-import networkx as nx
-import networkx.algorithms.community as nx_comm
 
-##################### CLUSTERING ON THE GRAPH ITSELF WITH THE WHOLE ONTOLOGY - NOT CORRECT #######################
-# parse the ontology into a graph
+#### create NetworkX graph ####
+
+# parse graph
 g = Graph()
-g.parse("./data/Project")
+g.parse("./data/KG.ttl")
 
 # turn graph into networkx graph
 nx_graph = rdflib_to_networkx_digraph(g)
@@ -39,7 +35,6 @@ nx_graph_nl = nx.relabel_nodes(nx_graph,map_dict,copy=True)
 # find owl classes
 EX = rdflib.Namespace("http://test.org/myonto.owl#")
 g.bind("ex", EX)
-#test = URIRef(EX+str(10016296))
 owl_classes = []
 for s,p,o in g.triples((None, None, OWL.Class)):
     owl_classes.append(s.split("#")[1])
@@ -56,37 +51,31 @@ for edge in nx.edges(nx_graph_nl):
     if edge[0] in subjects and edge[1] != 'nan':
         edges.append(edge)
 
-# # draw graph
-# plt.figure(3,figsize=(10,10), dpi=160) 
-# pos = nx.spring_layout(nx_graph_nl, k=.7)
-# nx.draw(nx_graph_nl, pos,
-#                  with_labels=True, nodelist = owl_classes,node_color = [range(11)],
-#                 node_size=50, font_size=4, edgelist = edges, alpha=0.6,linewidths=0.1,style=':' )
-# plt.show()
 
-# clustering
+#### clustering ####
+
+# find communities
 communities = nx_comm.louvain_communities(nx_graph_nl,resolution=1) #resolution: high number favor small communitites, low favor large communities
 print('Number of found communitites', len(communities),
 '\n Number of nodes in the graph',nx.number_of_nodes(nx_graph_nl))
+
 for i in range(len(communities)):
     print(f'{i}th community: \n',communities[i] )
+    
 
-# pos = nx.shell_layout(nx_graph_nl)
-# nx.draw(nx_graph_nl, pos, edge_color='k',font_weight='light', 
-#         node_size= 100, width= 0.8)
-# # plt.show()
+# draw whole graph
+pos = nx.shell_layout(nx_graph_nl)
+nx.draw(nx_graph_nl, pos, edge_color='k',font_weight='light', 
+        node_size= 100, width= 0.8)
+plt.show()
 
-# for com in communities:
-#     nx.draw_networkx_nodes(nx_graph_nl,
-#                            pos,
-#                            nodelist=com, 
-#                            #node_color=np.random.rand(3,),
-#                            node_color = range(len(com)),
-#                            label=True,
-#                            node_size=100)  
-# #plt.show()
+# draw communities
+for com in communities:
+    nx.draw_networkx_nodes(nx_graph_nl,
+                           pos,
+                           nodelist=com, 
+                           node_color = range(len(com)),
+                           label=True,
+                           node_size=100)  
+    plt.show()
 
-# for com in communities[:3]:
-#     nx.draw(nx_graph_nl, with_labels=True,
-#                 node_size=800, font_size=9 , font_color = "darkblue",nodelist = com, node_color=range(len(com)),alpha=0.6,linewidths=0.1,style=':')
-#     # plt.show()
